20d7dc82fe6a296d18486073eabb73b6669a2f53
baptiste <baptiste1.heraud@epitech.eu>
2025-02-15 20:58:54 +0100

    Create

--- toorna.py
+++ toorna.py
@@ -1,35 +1,44 @@
 import requests
 import sys
+import os
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ids = os.getenv("TOORNAMENTIDS")
+ids = ids.split(',')
+print("Démarrage du bot")
+print(ids)
+print(f"{len(ids)} tournois à scrapper")
 
 # URL de la page web
-url = "https://play.toornament.com/fr/tournaments/8541988438450577408/"
 
-# Effectuer une requête GET pour récupérer le contenu de la page
-response = requests.get(url)
+for event in ids:
+    url = "https://play.toornament.com/fr/tournaments/" + event + "/"
+    response = requests.get(url)
+    # Vérifier si la requête a réussi
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
 
-# Vérifier si la requête a réussi
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "html.parser")
+        # Trouver tous les éléments ayant la classe "size"
+        size_elements = soup.find(class_="size")
+        children = size_elements.find_all("div")
 
-    # Trouver tous les éléments ayant la classe "size"
-    size_elements = soup.find(class_="size")
-    children = size_elements.find_all("div")
-
-    first_child = children[0] if len(children) > 1 else None
-    second_child = children[1] if len(children) > 1 else None
+        first_child = children[0] if len(children) > 1 else None
+        second_child = children[1] if len(children) > 1 else None
 
 
-    discipline = soup.find(class_="discipline")
-    text = discipline.find('a', class_='highlighted').get_text().lower()
-    print(text)
+        discipline = soup.find(class_="discipline")
+        text = discipline.find('a', class_='highlighted').get_text().lower()
 
-    # Chercher les enfants avec la classe "current" dans chaque "size"
-    if first_child:
-        currentValue = first_child.text
-        print(currentValue)
-    if first_child:
-        currentMax = second_child.text
-        print(currentMax)
-else:
-    print("Erreur lors de la récupération de la page:", response.status_code)
+
+        # Chercher les enfants avec la classe "current" dans chaque "size"
+        if first_child:
+            currentValue = first_child.text
+        if first_child:
+            currentMax = second_child.text
+
+        print("{}: {} / {}".format(text, currentValue, currentMax))
+    else:
+        print("Erreur lors de la récupération de la page:", response.status_code)
